221849b8ec2c0645fda91d679133c2231e29a341
brayan.labrada@holbertonschool.com <brayan.labrada@holbertonschool.com>
2020-09-22 22:17:29 +0000

    prueba 1

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - Print "Holberton" followed by a new line.
+ * Return: 0
+ */
+
+int main(void)
+{
+char name[] = "Holberton";
+int i;
+
+for (i = 0; i < 9; i++)
+{
+_putchar(name[i]);
+}
+
+_putchar('\n');
+
+return (0);
+}
